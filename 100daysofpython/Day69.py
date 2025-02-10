195f71cddadae05e944772f8fa510f65f1f9f9d9
Ingryd Lorena <ingrydlorena766@gmail.com>
2025-02-10 16:28:53 -0300

    adding day 71 v1

--- 100daysofpython/Day69.py
+++ 100daysofpython/Day69.py
@@ -10,18 +10,14 @@ import plotly.express as px
 st.set_page_config(layout='wide')
 
 data = pd.read_csv('supermarket_sales.csv', sep=';', decimal=',')
+
 data['Date'] = pd.to_datetime(data["Date"])
 data = data.sort_values('Date')
-
-
 data['Month'] = data['Date'].apply(lambda x: str(x.year) + '-' + str(x.month))
+
 month = st.sidebar.selectbox('Month', data['Month'].unique())
-
-
 data_filtered = data[data['Month'] == month]
-
 city_total = data_filtered.groupby("City")[["Total"]].sum().reset_index()
-
 rating = data_filtered.groupby("City")[["Rating"]].mean().reset_index()
 
 
@@ -44,5 +40,5 @@ col4.plotly_chart(fig_kind, use_container_width=True)
 fig_rating = px.bar(data_filtered, x='City', y='Rating', title='Rating')
 col5.plotly_chart(fig_rating, use_container_width=True)
 
-print(data)
+
 
