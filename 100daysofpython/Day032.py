e274fa1f0017c7642dd9d8ff2319485ee1ceae50
Ingryd Lorena <160596486+ingrydlorena@users.noreply.github.com>
2025-05-06 21:25:49 -0300

    Delete 100daysofpython directory

--- 100daysofpython/Day032.py
+++
@@ -1,9 +0,0 @@
-'''
-Day 32: File operations: Read
-Read and display the contents of a text file.
-
-For this challenge you need create a file .txt to read them
-'''
-
-file = open("file.txt", 'r')
-print(file.read())
--- 100daysofpython/Day033.py
+++
@@ -1,9 +0,0 @@
-'''
-Day 33: File operations: Write
-Write data to a text file.
-'''
-another_file = open("another_file.txt", 'w')
-another_file.write("Hello World! \nThis is another file, you can read the file one yet")
-
-another_file = open("another_file.txt", 'r')
-print(another_file.read())
