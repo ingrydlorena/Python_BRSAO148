6989e4aed7eb8509081d2195ae738c35d5a8e2be
Ingryd Lorena <160596486+ingrydlorena@users.noreply.github.com>
2025-05-19 10:12:07 -0300

    Create exe_01.py

---
+++ AWS IA python/exercicio_2/exe_01.py
@@ -0,0 +1,21 @@
+'''1- Classificador de Idade
+Crie um programa que solicite a idade do usuário e classifique-o
+em uma das seguintes categorias:
+
+*Criança (0-12 anos),
+*Adolescente (13-17 anos),
+*Adulto (18-59 anos) ou
+*Idoso (60 anos ou mais).'''
+
+# Solicita a idade do usuário
+idade = int(input("Digite sua idade: "))
+
+# Classifica a idade
+if idade >= 0 and idade <= 12:
+    print("Criança")
+elif idade <= 17:
+    print("Adolescente")
+elif idade <= 59:
+    print("Adulto")
+else:
+    print("Idoso")
