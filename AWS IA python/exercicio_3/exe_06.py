4dd54e3f46b38e4b0951b51054bd4fba4409bf40
Ingryd Lorena <160596486+ingrydlorena@users.noreply.github.com>
2025-05-19 10:17:03 -0300

    Create exe_06.py

---
+++ AWS IA python/exercicio_3/exe_06.py
@@ -0,0 +1,13 @@
+'''6 - Crie um programa que simula a validação de uma senha usando um loop while True. O usuário tem no máximo 3 tentativas para acertar a senha correta. Use break para encerrar o loop quando a senha for correta ou quando o número máximo de tentativas for atingido.'''
+senha_correta = "senha123"
+tentativas = 0
+
+while True:
+    senha = input("Digite a senha: ")
+    tentativas += 1
+    if senha == senha_correta:
+        print("Acesso concedido")
+        break
+    elif tentativas == 3:
+        print("Número máximo de tentativas atingido")
+        break
