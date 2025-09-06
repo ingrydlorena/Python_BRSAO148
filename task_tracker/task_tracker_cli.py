55b2e1a902a45169169108ebd0f34fdff7d169e6
Ingryd <ingrydlorena@gmail.com>
2025-09-06 14:17:22 -0300

    "Começando o codigo"

---
+++ task_tracker/task_tracker_cli.py
@@ -0,0 +1,29 @@
+# Json import
+import json
+# inicializacion variables
+def __init__(self):
+    self.list_task = {'id':' ',
+                      'description':' ',
+                      'status':' ',
+                      'createdAt':' ',
+                      'updatedAt':' '}
+
+# Add task
+def add_task(self):
+    self.add_description = input("Enter your task:\n")
+    
+    ...
+
+# Update task
+
+# Delete task
+
+# Mark a task as in progress or done
+
+# list all task
+
+# list all task that are done
+
+# List all task that are not done
+
+# List all tasks that are in progress
